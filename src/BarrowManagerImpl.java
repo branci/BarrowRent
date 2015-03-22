204a1419071eb70fd07ab5e27ff6415df5666725
Medzikarp <mperichta@gmail.com>
2015-03-22 17:59:16 +0100

    Update BarrowManagerImpl.java

--- src/BarrowManagerImpl.java
+++ src/BarrowManagerImpl.java
@@ -24,18 +24,68 @@ public class BarrowManagerImpl implements BarrowManager {
     }
     
     @Override
-    public void deleteBarrow(Barrow barrow) throws ServiceFailureException {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void deleteBarrow(long barrowId) throws ServiceFailureException {
+        try (Connection conn = dataSource.getConnection()) {
+            try (PreparedStatement st = conn.prepareStatement("DELETE FROM barrow WHERE id=?")) {
+                st.setLong(1, barrowId);
+                if (st.executeUpdate() != 1) {
+                    throw new ServiceFailureException("did not delete barrow with id =" + barrowId);
+                }
+            }
+        } catch (SQLException ex) {
+            log.error("db connection problem", ex);
+            throw new ServiceFailureException("Error when retrieving all barrows", ex);
+        }
     }
     
     @Override
     public List<Barrow> findAllBarrows() throws ServiceFailureException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        log.debug("finding all barrows");
+        try (Connection conn = dataSource.getConnection()) {
+            try (PreparedStatement st = conn.prepareStatement("SELECT id,use,volumeLt FROM grave")) {
+                ResultSet rs = st.executeQuery();
+                List<Barrow> result = new ArrayList<>();
+                while (rs.next()) {
+                    result.add(resultSetToBarrow(rs));
+                }
+                return result;
+            }
+        } catch (SQLException ex) {
+            log.error("db connection problem", ex);
+            throw new ServiceFailureException("Error when retrieving all barrows", ex);
+        }
     }
     
     @Override
     public Barrow getBarrowById(Long id) throws ServiceFailureException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        try (Connection conn = dataSource.getConnection()) {
+            try (PreparedStatement st = conn.prepareStatement("SELECT id,use,volumeLt FROM barrow WHERE id = ?")) {
+                st.setLong(1, id);
+                ResultSet rs = st.executeQuery();
+                if (rs.next()) {
+                    Barrow barrow = resultSetToBarrow(rs);
+                    if (rs.next()) {
+                        throw new ServiceFailureException(
+                                "Internal error: More entities with the same id found "
+                                        + "(source id: " + id + ", found " + barrow + " and " + resultSetToBarrow(rs));
+                    }
+                    return barrow;
+                } else {
+                    return null;
+                }
+            }
+        } catch (SQLException ex) {
+            log.error("db connection problem", ex);
+            throw new ServiceFailureException("Error when retrieving all barrows", ex);
+        }
+    }
+    
+    private Barrow resultSetToBarrow(ResultSet rs) throws SQLException {
+        Barrow barrow = new Barrow();
+        barrow.setId(rs.getLong("id"));
+        barrow.setUse(rs.getString("use"));
+        barrow.setVolumeLt(rs.getDouble("volumeLt"));
+        return barrow;
     }
     
 }
